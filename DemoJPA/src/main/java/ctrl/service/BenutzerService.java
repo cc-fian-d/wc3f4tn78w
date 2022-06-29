2dd5409a30aa18bda5b24c342cc066b6d64745a7
W8Admin <W8Admin@host.docker.internal>
2022-06-29 14:49:40 +0200

    update

--- DemoJPA/src/main/java/ctrl/service/BenutzerService.java
+++ DemoJPA/src/main/java/ctrl/service/BenutzerService.java
@@ -8,6 +8,7 @@ import javax.inject.Inject;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import javax.transaction.UserTransaction;
 
 import model.Benutzer;
@@ -30,22 +31,45 @@ public class BenutzerService {
 	private UserTransaction ut;
 
 	@Inject
-	private Benutzer benutzer;
+	private Benutzer injectedBenutzer;
 
 	public List<Benutzer> holeAlleBenutzer() {
 		return em.createQuery("SELECT b FROM Benutzer b", Benutzer.class).getResultList();
 	}
+	
+	public List<Benutzer> findBenutzerByVorname(){
+//		return em.createQuery("SELECT b FROM Benutzer b WHERE b.vorname LIKE '%a%'", Benutzer.class).getResultList();
+		
+		TypedQuery<Benutzer> tq = em.createNamedQuery(Benutzer.QUERY_FIND_BY_VORNAME, Benutzer.class);
+		tq.setParameter(Benutzer.PARAMETER_FIND_BY_VORNAME, "%lia");	
+		return tq.getResultList();
+	}
+	
+	public List<Benutzer> findBenutzerByNachname(){
+		TypedQuery<Benutzer> tq = em.createNamedQuery(Benutzer.QUERY_FIND_BY_NACHNAME, Benutzer.class);
+		tq.setParameter(Benutzer.PARAMETER_FIND_BY_NACHNAME, "%ei%");	
+		return tq.getResultList();
+		
+		
+	}
 
 	public String persistBenutzer() {
 		System.out.println("persistBenutzer");
-		System.out.println(benutzer.getClass());
-
+		System.out.println(injectedBenutzer.getClass());
 		
-		//model.Benutzer$Proxy$_$$_WeldClientProxy
+		/**
+		 * OOP ->>> PMO
+		 */
+		Benutzer toPersist = new Benutzer();
+		toPersist.setPersonalnummer(injectedBenutzer.getPersonalnummer());
+		toPersist.setVorname(injectedBenutzer.getVorname());
+		toPersist.setNachname(injectedBenutzer.getNachname());
 		
 		try {
 			ut.begin();
-			em.persist(benutzer);
+//			em.persist(new Benutzer(benutzer));
+//			em.persist(benutzer.clone());
+			em.persist(toPersist);
 			ut.commit();
 		} catch (Exception e) {
 			System.out.println("EXCEPTION :" + e.getMessage());
--- DemoJPA/src/main/java/model/Benutzer.java
+++ DemoJPA/src/main/java/model/Benutzer.java
@@ -9,11 +9,25 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 
 @Entity
 @RequestScoped
 @Named("benutzer")
+@NamedQueries({
+		@NamedQuery(name = Benutzer.QUERY_FIND_BY_VORNAME, query = "SELECT b FROM Benutzer b WHERE b.vorname LIKE :"
+				+ Benutzer.PARAMETER_FIND_BY_VORNAME),
+		@NamedQuery(name = Benutzer.QUERY_FIND_BY_NACHNAME, query = "SELECT b FROM Benutzer b WHERE b.nachname LIKE :"
+				+ Benutzer.PARAMETER_FIND_BY_NACHNAME) })
+
 public class Benutzer implements Serializable {
+
+	public static final String QUERY_FIND_BY_VORNAME = "QUERY_FIND_BY_VORNAME";
+	public static final String PARAMETER_FIND_BY_VORNAME = "vorname";
+
+	public static final String QUERY_FIND_BY_NACHNAME = "QUERY_FIND_BY_NACHNAME";
+	public static final String PARAMETER_FIND_BY_NACHNAME = "nachname";
 
 	/**
 	 * 
@@ -49,6 +63,21 @@ public class Benutzer implements Serializable {
 
 	public void setNachname(String nachname) {
 		this.nachname = nachname;
+	}
+
+	public Benutzer() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Benutzer(Benutzer origin) {
+		this.setPersonalnummer(origin.getPersonalnummer());
+		this.setVorname(origin.getVorname());
+		this.setNachname(origin.getNachname());
+	}
+
+	@Override
+	public Benutzer clone() {
+		return new Benutzer(this);
 	}
 
 	@Override
